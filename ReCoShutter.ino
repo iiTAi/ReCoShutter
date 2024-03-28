22420ce6534e982b89d737d1ae7b90a12dd52d47
iiTAi <kimagureowata@icloud.com>
2024-03-29 00:33:06 +0900

    add setSelect

--- ReCoShutter.ino
+++ ReCoShutter.ino
@@ -23,8 +23,7 @@ OzOLED oled;
 uint8_t sp = 1;  // Set pointer
 uint8_t setFlag = 0;
 uint8_t cp = 2;  // Cursor pointer
-uint8_t sw;
-uint16_t value;
+uint8_t sw;  // switch
 
 uint8_t readSwitch() {
   uint16_t value = analogRead(PINB3);
@@ -92,9 +91,13 @@ void loop() {
       oled.printString("-");
 
       if (sw == 2 && cp != 2) {
+        oled.setCursorXY(0, cp);
+        oled.printChar(' ');
         cp--;
         beep();
       } else if (sw == 3 && cp != 3) {
+        oled.setCursorXY(0, cp);
+        oled.printChar(' ');
         cp++;
         beep();
       } else if (sw == 4) {
@@ -102,12 +105,51 @@ void loop() {
           case 2: mode = setSelect; break;
           case 3: mode = edit; break;
         }
-        cp = (cp == 3) ? 2 : 3;
+        cp = 2;
         oled.clearDisplay();
         beep();
       }
       break;
     case setSelect:
+      oled.setCursorXY(0, 0);
+      oled.printString("SELECT SET");
+      oled.setCursorXY(0, 1);
+      oled.printString("----------------");
+
+      oled.setCursorXY(1, 2);
+      oled.printString("SET");
+      oled.printNumber((long)sp);
+      oled.setCursorXY(1, 3);
+      oled.printString("RANGE : ");
+      oled.printNumber((long)set[sp - 1].range);
+      oled.setCursorXY(1, 4);
+      oled.printString("OFFSET: ");
+      oled.printNumber((long)set[sp - 1].offset);
+      oled.setCursorXY(1, 5);
+      oled.printString("LOOP  : ");
+      oled.printNumber((long)set[sp - 1].loop);
+      oled.setCursorXY(1, 6);
+      oled.printString("TIMINT: ");
+      oled.printNumber((long)set[sp - 1].interval);
+
+      if (sw == 1) {
+        mode = mainMenu;
+        sp = 1;
+        oled.clearDisplay();
+        beep();
+      } else if (sw == 2 && sp != 1) {
+        sp--;
+        oled.printChar(' ');
+        beep();
+      } else if (sw == 3 && sp != 3) {
+        sp++;
+        oled.printChar(' ');
+        beep();
+      } else if (sw == 4) {
+        mode = timer;
+        oled.clearDisplay();
+        beep();
+      }
       break;
     case timer:
       break;
